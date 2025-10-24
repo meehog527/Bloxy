9ec2ae2c32bdde469022f4740483c410b540c61a
meehog527 <52838950+meehog527@users.noreply.github.com>
2025-10-23 22:53:52 -0400

    update bluetooth_setup.py

--- src/bluetooth_setup.py
+++ src/bluetooth_setup.py
@@ -1,11 +1,19 @@
-from bluezero import peripheral
+from bluezero import peripheral, adapter
 from config import *
 from report_map import REPORT_MAP
+from logger import get_logger
 
 LOCAL_NAME = "Bloxy"
 
+logger = get_logger("bluetooth_setup")
+
+def get_adapter_address():
+    ad = adapter.Adapter()
+    return ad.address
+
 def create_peripheral():
-    ble = peripheral.Peripheral(adapter_address=None, local_name=LOCAL_NAME, appearance=961)
+    logger.debug("Creating Bluetooth peripheral...")
+    ble = peripheral.Peripheral(adapter_address=get_adapter_address(), local_name=LOCAL_NAME, appearance=961)
     ble.add_service(1, UUID_HID_SERVICE, primary=True)
     ble.add_characteristic(1, 1, UUID_HID_INFORMATION, bytes([0x11, 0x01, 0x00, 0x02]), ['read'])
     ble.add_characteristic(1, 2, UUID_HID_REPORT_MAP, REPORT_MAP, ['read'])
