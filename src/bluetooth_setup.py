34dc03df042e9dc1688d47c0bc692407623f21ee
meehog527 <52838950+meehog527@users.noreply.github.com>
2025-10-23 23:07:50 -0400

    update bluetooth_setup.py

--- src/bluetooth_setup.py
+++ src/bluetooth_setup.py
@@ -17,12 +17,12 @@ def create_peripheral():
     logger.debug("Creating Bluetooth peripheral...")
     ble = peripheral.Peripheral(adapter_address=get_adapter_address(), local_name=LOCAL_NAME, appearance=961)
     ble.add_service(1, UUID_HID_SERVICE, primary=True)
-    ble.add_characteristic(1, 1, UUID_HID_INFORMATION, bytes([0x11, 0x01, 0x00, 0x02]), ['read'])
-    ble.add_characteristic(1, 2, UUID_HID_REPORT_MAP, REPORT_MAP, ['read'])
-    ble.add_characteristic(1, 3, UUID_HID_PROTOCOL_MODE, bytes([0x01]), ['read', 'write-without-response'])
-    ble.add_characteristic(1, 4, UUID_HID_CONTROL_POINT, b'\x00', ['write-without-response'])
-    ble.add_characteristic(1, 5, UUID_REPORT, bytes([0x01] + [0x00]*8), ['read', 'notify'])
-    ble.add_characteristic(1, 6, UUID_REPORT, bytes([0x02, 0x00, 0x00, 0x00]), ['read', 'notify'])
+    ble.add_characteristic(1, 1, UUID_HID_INFORMATION, bytes([0x11, 0x01, 0x00, 0x02]), False, ['read'])
+    ble.add_characteristic(1, 2, UUID_HID_REPORT_MAP, REPORT_MAP, False, ['read'])
+    ble.add_characteristic(1, 3, UUID_HID_PROTOCOL_MODE, bytes([0x01]), False, ['read', 'write-without-response'])
+    ble.add_characteristic(1, 4, UUID_HID_CONTROL_POINT, b'\x00', False, ['write-without-response'])
+    ble.add_characteristic(1, 5, UUID_REPORT, bytes([0x01] + [0x00]*8), True, ['read', 'notify'])  # Keyboard
+    ble.add_characteristic(1, 6, UUID_REPORT, bytes([0x02, 0x00, 0x00, 0x00]), True, ['read', 'notify'])  # Mouse
     return ble
 
 def power_on_bluetooth():
