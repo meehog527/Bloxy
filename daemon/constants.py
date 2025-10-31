991bcbe9a13288bcd99e14e1d3102263d6b0c11b
meehog527 <52838950+meehog527@users.noreply.github.com>
2025-10-31 11:46:54 -0400

    debug

--- daemon/constants.py
+++ daemon/constants.py
@@ -1,3 +1,11 @@
+import logging
+
+# ================================
+# Logging
+# ================================
+LOG_LEVEL = logging.DEBUG
+LOG_FORMAT = "%(asctime)s [%(levelname)s] %(name)s.%(funcName)s: %(message)s"
+
 # ================================
 # BlueZ Core Constants
 # ================================
