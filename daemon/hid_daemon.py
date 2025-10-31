8edca733f85efafdb5e3a4d12b85933c7c8fea5b
meehog527 <52838950+meehog527@users.noreply.github.com>
2025-10-31 14:50:14 -0400

    debug

--- daemon/hid_daemon.py
+++ daemon/hid_daemon.py
@@ -412,7 +412,7 @@ def repl_loop(daemon):
             mac = cmd.split(" ", 1)[1]
             print("Trust result:", daemon.controller.trust_device(mac))
         elif cmd == "cache-list":
-            print(daemon.controller.list_cached_devices)
+            print(daemon.controller.list_cached_devices())
         elif cmd.startswith("cache-clear "):
             mac = cmd.split(" ", 1)[1]
             daemon.controller.clear_cached_devices(mac)
