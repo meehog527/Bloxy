a9aa46d0c1f8e0ae9401378fb8fadd86e4c60735
meehog527 <52838950+meehog527@users.noreply.github.com>
2025-11-02 00:37:36 -0400

    debug

--- daemon/evdev_tracker.py
+++ daemon/evdev_tracker.py
@@ -38,13 +38,14 @@ class EvdevTracker:
                     if event.type == ecodes.EV_KEY:
                         key_event = categorize(event)
                         keycode = key_event.keycode
-                        print(f"event: {key_event}, code: {keycode}" )
+                        #print(f"event: {key_event}, code: {keycode}" )
                         if isinstance(keycode, list):
                             keycode = keycode[0]
                         if key_event.keystate == key_event.key_down:
-                            print(f"key down: {key_event}, code: {keycode}" )
+                            #print(f"key down: {key_event}, code: {keycode}" )
                             if str(keycode).startswith('BTN_'):
                                 self.buttons.add(keycode)
+                                print(f"key down: {key_event}, code: {keycode}" )
                             else:
                                 self.pressed_keys.add(keycode)
                         elif key_event.keystate == key_event.key_up:
