d28263c9951cb3aa78575d98af60f66ca78d4015
meehog527 <52838950+meehog527@users.noreply.github.com>
2025-11-04 14:29:29 -0500

    debug

--- daemon/hid_reports.py
+++ daemon/hid_reports.py
@@ -69,15 +69,13 @@ class HIDReportBuilder:
         if hasattr(self, '_last_pos') and self._last_pos is not None:
             dx = int(rel_x) - self._last_pos[0]
             dy = int(rel_y) - self._last_pos[1]
-            dv = scroll_v
 
-            if (scroll_v > 0 and self._last_pos[2] < 0) or (scroll_v < 0 and self._last_pos[2] > 0) :   
-                scroll_v = 0
 
         else:
             dx = int(rel_x)
             dy = int(rel_y)
-            dv = int(scroll_v)
+            
+        dv = int(scroll_v)
 
         # update last_pos to the current absolute values so next call computes a delta
         self._last_pos = (int(rel_x), int(rel_y), int(dv))
