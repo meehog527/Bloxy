66884ae1e1f928fbe629c603178d02b61cfd8ebf
meehog527 <52838950+meehog527@users.noreply.github.com>
2025-11-04 14:20:38 -0500

    debug

--- daemon/hid_reports.py
+++ daemon/hid_reports.py
@@ -69,12 +69,20 @@ class HIDReportBuilder:
         if hasattr(self, '_last_pos') and self._last_pos is not None:
             dx = int(rel_x) - self._last_pos[0]
             dy = int(rel_y) - self._last_pos[1]
+            
+            if (scroll_v > 0 and self._last_post[2] < 0) or (scroll_v < 0 and self._last_post[2] > 0) :
+                dv = scroll_v
+                scroll_v = 0
+
         else:
             dx = int(rel_x)
             dy = int(rel_y)
+            scroll_v = int(scroll_v)
+
+  
 
         # update last_pos to the current absolute values so next call computes a delta
-        self._last_pos = (int(rel_x), int(rel_y), int(scroll_v))
+        self._last_pos = (int(rel_x), int(rel_y), int(dv))
 
         # clamp to signed 8-bit and convert to two's complement byte
         def to_signed_byte(v):
@@ -86,8 +94,8 @@ class HIDReportBuilder:
 
         report[1] = to_signed_byte(dx)
         report[2] = to_signed_byte(dy)
-        report[3] = scroll_v
+        report[3] = dv
 
-        print(scroll_v)
+        print(dv)
 
         return report
